03b314d467eeb8e7f581f6320c7a149a6e72faa7
Chinyerem Emmanuel <kelvinesthy@gmail.com>
2020-10-01 00:09:56 +0100

    stage 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,15 +3,23 @@ import { Http, Response } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 
 import { Album } from "./album";
+import { Product } from "./product";
 
 import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService {
-
+  private _privatesUrl = '../assets/products.json'
   private _albumUrl = '../assets/album.json';
+
+
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(){
+    this._http.get(this._privatesUrl).map((response) => <Product[]>response.json());
+    return Observable<Product[]>
   }
 }
