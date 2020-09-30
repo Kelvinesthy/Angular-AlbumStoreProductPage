c536bf30a81a5dedee47106ecd3a192839a28ab4
Chinyerem Emmanuel <kelvinesthy@gmail.com>
2020-09-30 04:22:02 +0100

    stage 4 update2

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,8 @@
 import { Track } from "./track";
 
-export interface IAlbum {
-  name : string,
-  releaseDate: string,
-  coverImage: string,
-  tracks: ITrack[]
+export interface Album {
+  name : string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[]
 }
